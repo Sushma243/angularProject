76a5f0c90805193da1bde649d057768664433b67
sushma Kudum <sushma243.kudum@gmail.com>
2022-02-20 21:49:06 +0530

    javascript open() close() checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularProject';
+  targetedWindow: any;
+
+//opens up yahoo here using current window object
+   openWindow():any
+  {
+   this.targetedWindow=window.open("https://in.yahoo.com","","height=30px;width=40px;left=40px;top=45px");
+  }
+
+  //close will close the window which got opened above
+  closeWindow():void
+  {
+    this.targetedWindow.close();
+  }
 }
